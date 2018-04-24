22ed82460d8815c81563573c368e6e94335f49ab
serex <alexandre.serex@epfl.ch>
2018-04-24 10:49:11 +0200

    color change

--- portfolio/imports/ui/Components/ArticlePortion.jsx
+++ portfolio/imports/ui/Components/ArticlePortion.jsx
@@ -288,5 +288,5 @@ const Caption = styled('div')`
     padding-bottom: 50px;
     margin: auto 10% !important;
     font-style: italic;
-    color: rgb(100, 100, 100);
+    color: rgb(200, 200, 200);
 `;
